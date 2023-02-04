1f1b84247c2137910cd970488b576934e0ac8ae9
Umagayathri <u541732@bpost.be>
2023-02-04 14:54:19 +0100

    UAT code commited

---
+++ terraform/UAT_acct.tf
@@ -0,0 +1,23 @@
+module "uat_acct" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "uatacct2023@tcs.com"
+    AccountName               = "Acceptance"
+    ManagedOrganizationalUnit = "AFT_ACCT"
+    SSOUserEmail              = "umagayathri.vasu@tcs.com"
+    SSOUserFirstName          = "Acceptance"
+    SSOUserLastName           = "AFT"
+  }
+  account_tags = {
+    "Account_Name" = "Acceptance"
+  }
+  change_management_parameters = {
+    change_requested_by = "Umagayathri"
+    change_reason       = "Acceptance account creation"
+  }
+  custom_fields = {
+    group = "Acceptance"
+  }
+  account_customizations_name = "Acceptance"
+}

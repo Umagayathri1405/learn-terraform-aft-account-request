42df545b35b440d94d48034ed505a275250eab15
V Umagayathri (ext) <u541732@bpost.be>
2023-03-08 05:12:33 +0100

    Acceptance_01 acct added

---
+++ terraform/UAT_acct_01.tf
@@ -0,0 +1,23 @@
+module "uat_acct" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "uatacct2023@gmail.com"
+    AccountName               = "Acceptance_01"
+    ManagedOrganizationalUnit = "AFT_ACCT"
+    SSOUserEmail              = "umagayathri.vasu@tcs.com"
+    SSOUserFirstName          = "Acceptance_01"
+    SSOUserLastName           = "AFT"
+  }
+  account_tags = {
+    "Account_Name" = "Acceptance_01"
+  }
+  change_management_parameters = {
+    change_requested_by = "Umagayathri"
+    change_reason       = "Second Acceptance account creation"
+  }
+  custom_fields = {
+    group = "Acceptance_01"
+  }
+  account_customizations_name = "Acceptance"
+}
